05316411cc9e66795f1123b5f9720b4fe8a102e8
jamesshep64AM <jamesshep@tamu.edu>
2025-01-29 01:37:53 +0000

    book collection 1 completed

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to @book, notice: "Book was successfully created." }
+        format.html { redirect_to books_path, notice: "Book was successfully created." }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to @book, notice: "Book was successfully updated." }
+        format.html { redirect_to books_path, notice: "Book was successfully updated." }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,14 +49,22 @@ class BooksController < ApplicationController
 
   # DELETE /books/1 or /books/1.json
   def destroy
-    @book.destroy
+    
+    #@book.destroy
 
-    respond_to do |format|
-      format.html { redirect_to books_path, status: :see_other, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    #respond_to do |format|
+    #  format.html { redirect_to books_path, status: :see_other, notice: "Book was successfully destroyed." }
+    #  format.json { head :no_content }
+    #end
+    
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "Book was successfully destroyed."
   end
 
+  def delete
+    @book = Book.find(params[:id])
+  end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_book
